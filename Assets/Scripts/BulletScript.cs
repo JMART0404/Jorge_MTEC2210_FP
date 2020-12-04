1f70e36514e84567ad500c474046641c21b786cf
JMART0404 <47122934+JMART0404@users.noreply.github.com>
2020-12-03 19:11:22 -0500

    Create BulletScript.cs

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Transform bullet;
+    public float speed;
+    public GameObject Bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bullet.position += Vector3.up * speed;
+
+        if (bullet.position.y >= 10)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+       void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.tag == "Enemy")
+            {
+                Destroy(other.gameObject);
+                Destroy(Bullet); 
+                // Player Score++
+            }
+        }
+    
+}
